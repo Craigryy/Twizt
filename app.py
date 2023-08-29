04f0a33a23c7ae5bf320b5294e006753900409cd
craigryy <harriajames97@gmail.com>
2023-08-29 17:23:16 +0100

    documented my app.py

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
+# Import necessary modules and classes
 from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
-from bson import ObjectId  # Import ObjectId from bson module
+from bson import ObjectId
 
+# Create a Flask web application instance
 app = Flask(__name__, template_folder='templates')
+
+# Configure the MongoDB connection URI
 app.config["MONGO_URI"] = "mongodb://localhost:27017/todo"
+
+# Create a PyMongo instance using the Flask app
 mongo = PyMongo(app)
 
-@app.route('/')
+# Define a route for the homepage ("/")
 def index():
+    # Retrieve tasks from the MongoDB database
     tasks = mongo.db.tasks.find()
+    # Render the "index.html" template with the tasks
     return render_template('index.html', tasks=tasks)
 
-@app.route('/add_task', methods=['POST'])
+# Define a route for adding tasks ("/add_task")
 def add_task():
+    # Get the task description from the form submitted via POST
     task_description = request.form.get('task_description')
+    # Insert the task into the MongoDB database
     mongo.db.tasks.insert_one({'description': task_description, 'completed': False})
+    # Redirect back to the homepage
     return redirect('/')
 
-@app.route('/update_task/<task_id>', methods=['POST'])
+# Define a route for updating task completion status ("/update_task/<task_id>")
 def update_task(task_id):
-    task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})  # Convert task_id to ObjectId
+    # Find the task in the MongoDB database using the task_id (converted to ObjectId)
+    task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
+    # Toggle the completion status of the task
     new_status = not task['completed']
+    # Update the task's completion status in the database
     mongo.db.tasks.update_one({'_id': ObjectId(task_id)}, {'$set': {'completed': new_status}})
+    # Redirect back to the homepage
     return redirect('/')
 
-@app.route('/delete_task/<task_id>', methods=['POST'])
+# Define a route for deleting a task ("/delete_task/<task_id>")
 def delete_task(task_id):
-    mongo.db.tasks.delete_one({'_id': ObjectId(task_id)})  # Convert task_id to ObjectId
+    # Delete the task from the MongoDB database using the task_id (converted to ObjectId)
+    mongo.db.tasks.delete_one({'_id': ObjectId(task_id)})
+    # Redirect back to the homepage
     return redirect('/')
 
+
+# Run the Flask app only if the script is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
